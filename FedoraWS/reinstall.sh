ddd517b713fac2b723e17adb53b5e7c658f64046
Ramiraz80 <eric@bellaiche.net>
2023-08-06 21:26:58 +0200

    First Commit, FedoraWS reinstall

---
+++ FedoraWS/reinstall.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# enable contrib and nonfree repositories
+sudo apt-add-repository contrib -y
+sudo apt-add-repository non-free -y
+
+# install ansible-core if not already installed
+if [ ! -f /usr/bin/ansible ]; 
+then
+    sudo dnf install -y ansible-core;
+    echo "Ansible-core has been installed, running install playbook";
+else
+    echo "Ansible-core already installed, running playbook"
+fi
+
+# install the community general flatpak module
+ansible-galaxy collection install community.general
+
+# run the apt install playbook
+if [ -f /usr/bin/ansible ]; 
+then
+    ansible-playbook reinstall-playbook.yml -K;
+fi
+
+# Set up git config user
+git config --global user.name "Ramiraz80"
+git config --global user.email "eric@bellaiche.net"
+
+# Check for distribution
+DISTRO=$(lsb_release -i | grep Distributor | cut -f 2-)
+
+# setup firewall with ufw, if Distribution is Debian
+if [ "Debian" = $DISTRO ];
+then
+    sudo ufw default deny incoming
+    sudo ufw default allow outgoing
+    sudo ufw enable
+fi
+
+
+
+#### Missing:
+# better lock screen and i3lock
