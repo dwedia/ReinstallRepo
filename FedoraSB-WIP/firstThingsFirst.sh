31069df4153d3a7dc1e801684f616073118ca752
Ramiraz80 <eric@bellaiche.net>
2023-08-08 17:32:20 +0200

    forgot a #. added now

--- FedoraSB-WIP/firstThingsFirst.sh
+++ FedoraSB-WIP/firstThingsFirst.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Since Silverblue uses rpm-ostree, not dnf, we need to use that to install a few packages, that wont run in Distrobox.
+# Since Silverblue uses rpm-ostree, not dnf, we need to use that to install a few packages, that wont run in Distrobox.
 
 # Install packages
 rpm-ostree install i3 ansible-core distrobox terminator gnome-shell-extension-pop-shell vim lxpolkit https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
