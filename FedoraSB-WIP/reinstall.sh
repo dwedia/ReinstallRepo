d8c318ddce1d5142e01fa4cfc86845e29281a88e
Ramiraz80 <eric@bellaiche.net>
2023-08-08 17:36:51 +0200

    moved git config to run before ansible

--- FedoraSB-WIP/reinstall.sh
+++ FedoraSB-WIP/reinstall.sh
@@ -3,15 +3,15 @@
 # install the community general flatpak module
 ansible-galaxy collection install community.general
 
+# Set up git config user
+git config --global user.name "Ramiraz80"
+git config --global user.email "eric@bellaiche.net"
+
 # run the apt install playbook
 if [ -f /usr/bin/ansible ]; 
 then
     ansible-playbook reinstall-playbook.yml -K;
 fi
-
-# Set up git config user
-git config --global user.name "Ramiraz80"
-git config --global user.email "eric@bellaiche.net"
 
 # Check for distribution
 DISTRO=$(cat /etc/os-release | grep PRETTY | awk -F '"' '{print $2}' | awk '{print $1}')
