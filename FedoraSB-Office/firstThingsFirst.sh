fe7c1e4bc0403a775254dc9d9350bfa16a371fea
Ramiraz80 <eric@bellaiche.net>
2023-11-20 10:50:11 +0100

    .

--- FedoraSB-Office/firstThingsFirst.sh
+++ FedoraSB-Office/firstThingsFirst.sh
@@ -7,7 +7,7 @@ ostreeStatus=$(rpm-ostree status | grep State | awk '{print $2}')
 
 if [ "$ostreeStatus" == "idle" ]; then
     # Install packages
-    rpm-ostree install i3 rofi ansible-core distrobox terminator vim lxpolkit https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    rpm-ostree install i3 rofi blueman ansible-core distrobox terminator vim lxpolkit https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
     echo "Rebooting, to ensure that the new packages are installed"
     sleep 5
     systemctl reboot
