c3dc26df74b5f4a72fae79f8a4485d31976034f1
Ramiraz80 <eric@bellaiche.net>
2023-11-21 12:05:00 +0100

    .

--- FedoraWS-Office/reinstall.sh
+++ FedoraWS-Office/reinstall.sh
@@ -19,21 +19,5 @@ then
 fi
 
 # Set up git config user
-git config --global user.name "Ramiraz80"
+git config --global user.name "dwedia"
 git config --global user.email "eric@bellaiche.net"
-
-# Check for distribution
-DISTRO=$(cat /etc/os-release | grep PRETTY | awk -F '"' '{print $2}' | awk '{print $1}')
-
-# setup firewall with ufw, if Distribution is Debian
-if [ "Debian" = $DISTRO ];
-then
-    sudo ufw default deny incoming
-    sudo ufw default allow outgoing
-    sudo ufw enable
-fi
-
-
-
-#### Missing:
-# better lock screen and i3lock
